6208c4eb1589e23ca5ecb2e22d49d0fb94e13194
gaidarvu <63213304+gaidarvu@users.noreply.github.com>
2024-10-29 00:41:55 +0300

    Add files via upload

---
+++ setup_repo.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+REPO_PATH=/opt/shvirtd-example-python
+
+# get repo
+git clone https://github.com/gaidarvu/shvirtd-example-python.git $REPO_PATH
+
+# run project
+cd $REPO_PATH
+docker compose up -d
+cd
